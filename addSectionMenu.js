154b3f523441d02037817e98c2921cb9c2f0dc0e
fahdrahali <rahali.fahd@gmail.com>
2025-01-03 15:00:58 +0100

    create a module to the section menu

---
+++ addSectionMenu.js
@@ -0,0 +1,46 @@
+const addSectionMenu = () => {
+  // create menu section
+  const menuSection = document.createElement('section');
+  menuSection.classList.add('menu');
+
+  // create img
+  const closeImg = document.createElement('img');
+  closeImg.src = './assets/close.png';
+  closeImg.alt = 'close';
+
+  // create button close
+  const closeBtn = document.createElement('button');
+  closeBtn.classList.add('close');
+  closeBtn.appendChild(closeImg);
+  menuSection.appendChild(closeBtn);
+
+  // create unordered list
+  const ul = document.createElement('ul');
+
+  // create list items function
+  const createListItems = (text, href) => {
+    let item = document.createElement('li');
+    let link = document.createElement('a');
+    link.textContent = text;
+    link.href = href;
+    item.appendChild(link);
+    ul.appendChild(item);
+  };
+
+  // create list items
+  createListItems('Portfolio', '#works');
+  createListItems('About', '#about');
+  createListItems('Contact', '#contact');
+
+  menuSection.appendChild(ul);
+
+  // append menu section to the top of body
+  document.body.insertBefore(menuSection, document.body.firstChild);
+
+  // add event listener 'click' to close button
+  closeBtn.addEventListener('click', () => {
+    document.body.removeChild(menuSection);
+  });
+};
+
+export default addSectionMenu;
