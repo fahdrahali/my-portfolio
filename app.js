5316db6c2b30f947b4bf56dc56abab33f949d400
fahdrahali <rahali.fahd@gmail.com>
2025-01-03 16:50:07 +0100

    create a project class and projects module

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-import addSectionMenu from './addSectionMenu.js';
+import addSectionMenu from './modules/addSectionMenu.js';
 
 // add event listener 'click' to union button
 const unionBtn = document.querySelector('.union');
 unionBtn.addEventListener('click', () => {
   console.log('union button clicked');
-  
   addSectionMenu();
 });
---
+++ modules/project.js
@@ -0,0 +1,13 @@
+class Project {
+  constructor(name, description, image, technologies, year, type, company) {
+    this.name = name;
+    this.company= company;
+    this.description = description;
+    this.image = image;
+    this.technologies = technologies;
+    this.year = year;
+    this.type = type;
+  }
+}
+
+export default Project;
