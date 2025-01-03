84ad4f6dcd6335d094e0c912f5ab2e76562aa6d5
fahdrahali <rahali.fahd@gmail.com>
2025-01-03 12:34:36 +0100

    create a menu section with javascript

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
+addSectionMenu = () => {
+  // create menu section
+  const menuSection = document.createElement('section');
+  menuSection.classList.add('menu');
+
+  // create img
+  const closeImg = document.createElement('img');
+  closeImg.src = './assets/close.png';
+  closeImg.alt = 'close';
+
+  // create button close
+  const closeBtn = document.createElement('button');
+  closeBtn.classList.add('close');
+  closeBtn.appendChild(closeImg);
+  menuSection.appendChild(closeBtn);
+
+  // create unordered list
+  const ul = document.createElement('ul');
+
+  // create list items function
+  const createListItems = (text, href) => {
+    item = document.createElement('li');
+    link = document.createElement('a');
+    link.textContent = text;
+    link.href = href;
+    item.appendChild(link);
+    ul.appendChild(item);
+  };
+
+  // create list items
+  createListItems('Portfolio', '#works');
+  createListItems('About', '#about');
+  createListItems('Contact', '#contact');
+
+  menuSection.appendChild(ul);
+
+  // append menu section to the top of body
+  document.body.insertBefore(menuSection, document.body.firstChild);
+
+  // add event listener 'click' to close button
+  closeBtn.addEventListener('click', () => {
+    document.body.removeChild(menuSection);
+  });
+};
+// add event listener 'click' to union button
+
 const unionBtn = document.querySelector('.union');
-const menu = document.querySelector('.menu');
-
-
 unionBtn.addEventListener('click', () => {
-  console.log('Union button clicked');
-  menu.classList.toggle('show');
+  addSectionMenu();
 });
-
-
-const closeBtn = document.querySelector('.close');
-
-closeBtn.addEventListener('click', () => {  
-  console.log('Close button clicked');
-  menu.classList.remove('show');
-}); 
-
