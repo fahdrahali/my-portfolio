b5d9c28998ab692c14df2bbe15848efe41b39f62
fahdrahali <rahali.fahd@gmail.com>
2025-01-08 18:09:24 +0100

    add a condition media screen to the event button see project

--- modules/projectSection.js
+++ modules/projectSection.js
@@ -1,83 +1,91 @@
-import addPrjectDetailsPopup from "./projectDetails.js";
-import projects from "./projects.js";
+import addPrjectDetailsDesktopPopup from './desktopPopup.js';
+import addPrjectDetailsMobilePopup from './projectDetails.js';
+import projects from './projects.js';
 
 const worksSection = document.createElement('section');
 worksSection.id = 'works';
 
-
 const createWorksCard = (project) => {
-    const worksCard = document.createElement('div');
-    worksCard.classList.add('works-card');
+  const worksCard = document.createElement('div');
+  worksCard.classList.add('works-card');
 
-    // create an image tag 
-    const img = document.createElement('img');
-    img.src = project.image;
-    img.alt = project.name
+  // create an image tag
+  const img = document.createElement('img');
+  img.src = project.image;
+  img.alt = project.name;
 
-    // create a div for works card block
-    const worksCardBlock = document.createElement('div');
-    worksCardBlock.classList.add('works-card-block');
+  // create a div for works card block
+  const worksCardBlock = document.createElement('div');
+  worksCardBlock.classList.add('works-card-block');
 
-    // create a h1 tag for project name
-    const h1 = document.createElement('h1');
-    h1.textContent = project.name;
+  // create a h1 tag for project name
+  const h1 = document.createElement('h1');
+  h1.textContent = project.name;
 
-    // create an unordred list for project infos
-    const ul = document.createElement('ul');
-    ul.classList.add('works-card-info');
+  // create an unordred list for project infos
+  const ul = document.createElement('ul');
+  ul.classList.add('works-card-info');
+  const li = document.createElement('li');
+  li.textContent = project.company;
+  ul.appendChild(li);
+
+  const li2 = document.createElement('li');
+  li2.textContent = project.type;
+  ul.appendChild(li2);
+
+  const li3 = document.createElement('li');
+  li3.textContent = project.year;
+  ul.appendChild(li3);
+
+  // create a p tag for project description
+  const p = document.createElement('p');
+  p.textContent = project.description;
+
+  // create an unordred list for project technologies
+  const ulTech = document.createElement('ul');
+  ulTech.classList.add('works-card-tech');
+  project.technologies.map((tech) => {
     const li = document.createElement('li');
-    li.textContent = project.company;
-    ul.appendChild(li);
+    li.textContent = tech;
+    ulTech.appendChild(li);
+  });
 
-    const li2 = document.createElement('li');
-    li2.textContent = project.type;
-    ul.appendChild(li2);
+  // create a button for project link
+  const btn = document.createElement('button');
+  btn.textContent = 'See Project';
+  btn.id = 'project-details';
 
-    const li3 = document.createElement('li');
-    li3.textContent = project.year;
-    ul.appendChild(li3);
+  // Create a MediaQueryList object
 
-    // create a p tag for project description
-    const p = document.createElement('p');
-    p.textContent = project.description;
 
-    // create an unordred list for project technologies
-    const ulTech= document.createElement('ul');
-    ulTech.classList.add('works-card-tech');
-    project.technologies.map(tech => {
-        const li = document.createElement('li');
-        li.textContent = tech;
-        ulTech.appendChild(li);
-    });
+  btn.addEventListener('click', () => {
+    const mobileScreen = window.matchMedia('(max-width: 768px)');
 
-    // create a button for project link
-    const btn = document.createElement('button');   
-    btn.textContent = 'See Project';
-    btn.id = 'project-details';  
-    
-    btn.addEventListener('click', () => {
-        console.log('button clicked', project);
-        addPrjectDetailsPopup(project);
-    }); 
+    if(mobileScreen.matches)
+    addPrjectDetailsMobilePopup(project);
 
-    // append all elements to works card
-    worksCard.appendChild(img);
-    worksCard.appendChild(worksCardBlock);
-    worksCardBlock.appendChild(h1);
-    worksCardBlock.appendChild(ul);
-    worksCardBlock.appendChild(p);
-    worksCardBlock.appendChild(ulTech);
-    worksCardBlock.appendChild(btn);
+    addPrjectDetailsDesktopPopup(project);
+  });
 
-    worksSection.appendChild(worksCard);
+  // append all elements to works card
+  worksCard.appendChild(img);
+  worksCard.appendChild(worksCardBlock);
+  worksCardBlock.appendChild(h1);
+  worksCardBlock.appendChild(ul);
+  worksCardBlock.appendChild(p);
+  worksCardBlock.appendChild(ulTech);
+  worksCardBlock.appendChild(btn);
 
-}
+  worksSection.appendChild(worksCard);
+};
 
 const addProjectsSection = () => {
-    projects.map(project => {
-        createWorksCard(project);
-    });
-    document.querySelector('.main').insertBefore(worksSection, document.getElementById('about'));
-}
+  projects.map((project) => {
+    createWorksCard(project);
+  });
+  document
+    .querySelector('.main')
+    .insertBefore(worksSection, document.getElementById('about'));
+};
 
 export default addProjectsSection;
