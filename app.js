453d3d8eb08731652f8b7f519c641dfa6bd136ad
fahdrahali <rahali.fahd@gmail.com>
2025-01-09 14:21:03 +0100

    add a click event for the submit button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import addSectionMenu from './modules/addSectionMenu.js';
+import validateEmailInput from './modules/formValidators.js';
 import addProjectsSection from './modules/projectSection.js';
 
 // add event listener 'click' to union button
@@ -9,4 +10,12 @@ unionBtn.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   addProjectsSection();
+});
+
+document.getElementById('get-in-touch').addEventListener('click', () => {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const msg = validateEmailInput(email);
+  if (msg) document.querySelector('.error-msg').innerText = msg;
 });
