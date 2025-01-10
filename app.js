c0f366cdec900da8019c45647bd97e16dd79172f
fahdrahali <rahali.fahd@gmail.com>
2025-01-10 11:44:02 +0100

    get the data form on load from the local storage

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 import addSectionMenu from './modules/addSectionMenu.js';
-import validateEmailInput from './modules/formValidators.js';
+import submitForm from './modules/formValidators.js';
 import addProjectsSection from './modules/projectSection.js';
+import User from './modules/user.js';
 
 // add event listener 'click' to union button
 const unionBtn = document.querySelector('.union');
+let email = document.getElementById('email');
+let name = document.getElementById('fname');
+let message = document.getElementById('msg');
+
 unionBtn.addEventListener('click', () => {
   addSectionMenu();
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   addProjectsSection();
+  User.getUserLocalStorage(name, email, message);
 });
 
 document.getElementById('get-in-touch').addEventListener('click', () => {
   event.preventDefault();
 
-  const email = document.getElementById('email').value;
-  const msg = validateEmailInput(email);
-  if (msg) document.querySelector('.error-msg').innerText = msg;
+  submitForm(name.value, email.value, message.value);
 });
